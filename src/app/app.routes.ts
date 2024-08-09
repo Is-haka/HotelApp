26903b82dfb02ce0bc718d890f4b1ca0ee2f1f35
is-haka <isihakamabo5@gmail.com>
2024-08-09 20:26:13 +0300

    added index route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { IndexComponent } from './start/index/index.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'start/index', pathMatch: 'full'},
+  {path: 'start/index', component: IndexComponent}
+];

c1767a0e3b064977792a19c1e07e4b0338be2b27
is-haka <isihakamabo5@gmail.com>
2024-08-09 20:27:51 +0300

    remove testing code for landing component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'HotelApp';
-}
+export class AppComponent {}
